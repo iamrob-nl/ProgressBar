c56339a485db6b5323aa3c6cdd5460c0f2067ed7
rob duits <rob.duits@incentro.com>
2016-10-25 15:10:43 +0200

    Release v1.0.1
    updated widget for new release

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-// Generated on 2016-10-12 using generator-mendix 1.3.3 :: git+https://github.com/mendix/generator-mendix.git
+// Generated on 2016-10-25 using generator-mendix 1.3.3 :: git+https://github.com/mendix/generator-mendix.git
 /*jshint -W069*/
 /*global module*/
 "use strict";
